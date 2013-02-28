6161f064ad3ef8d57d74619efc13404ecb0c4819
Justin Tout <jtt35@case.edu>
2013-02-28 12:17:35 -0500

    Finished level select. 2 levels in.

--- Assets/Menu Assets/LevelSelect/ChooseLevel.cs
+++ Assets/Menu Assets/LevelSelect/ChooseLevel.cs
@@ -1,15 +1,21 @@
+/* Justin Tout
+ * Handles the selection of levels. */
+
 using UnityEngine;
 using System.Collections;
 
 public class ChooseLevel : MonoBehaviour {
 	
-	private Color mar = new Color(.47F, .106F, .106F);
-	private Collider next;
-	private Collider last;
-	private GameObject main;
+	//Necessary fields 
+	private Color mar = new Color(.47F, .106F, .106F); //for scrollover
+	private Collider next; //to move right
+	private Collider last; //to move left 
+	private GameObject main; //the camera
+	public int totlevels;
 	
 	void Start()
 	{
+		//find everything on start
 		next = GameObject.Find ("NEXT").GetComponent<Collider>();
 		last = GameObject.Find("LAST").GetComponent<Collider>();
 		main = GameObject.FindGameObjectWithTag ("MainCamera");
@@ -17,20 +23,22 @@ public class ChooseLevel : MonoBehaviour {
 		
 	void OnMouseOver()
 	{
-		renderer.material.color = mar;
+		renderer.material.color = mar; //change color on mouseover 
 	}
 	
 	void OnMouseExit()
 	{
-		renderer.material.color = Color.white;
+		renderer.material.color = Color.white; //change back when necessary
 	}
 	
 	void OnMouseDown()
 	{
-		Debug.Log ("Movin'");
-		if (collider == next) //&& main.transform.position.x <= LevelSelector.totlevels - 1 * 30)
+		//Debug.Log ("Movin'");
+		//move right if next is pressed. 
+		if (collider == next && main.transform.position.x < (totlevels + 1) * 30) //to only show up to "coming soon" text
 			main.transform.Translate(new Vector3(30,0,0));
-		if (collider == last)// && main.transform.position.x >= 5)
+		//move left is last is pressed and not at tutorial
+		if (collider == last && main.transform.position.x > 0)
 			main.transform.Translate(new Vector3(-30,0,0));
 	}
 }
